a46477972b55179e53ba11e6a1108b6ad1433367
Sadia <mst.sadia01891@gmail.com>
2025-02-13 15:56:23 +0600

    'done'

---
+++ src/firebase/firebase.config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMSgglExj4XxZUmrLYzFlTFXnQV3uHwFI",
+  authDomain: "restaurant-3862a.firebaseapp.com",
+  projectId: "restaurant-3862a",
+  storageBucket: "restaurant-3862a.firebasestorage.app",
+  messagingSenderId: "706592841812",
+  appId: "1:706592841812:web:333bdecc370abd0fba8dc1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
